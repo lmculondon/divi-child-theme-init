73daf6ba22c8018a9a501bebf05a6d02aedd518c
lmculondon <cu.info@creditunion.co.uk>
2021-03-17 11:24:16 +0000

    Rebuild for Gulp

---
+++ copy/functions.php
@@ -0,0 +1,80 @@
+<?php
+/* 
+SCRIPTS AND STYLES 
+*/ 
+
+//Divi Parent Styles
+function theme_enqueue_styles() {
+	  wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
+	  wp_enqueue_style( 'lmcu-child', get_stylesheet_directory_uri() . '/css/style.css', array(divi));
+      wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), true );
+	}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+//TrustPilot
+function trustpilot_enqueue_scripts() {
+	wp_enqueue_script( 'trustpilot', '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js', array(),false, true);
+}
+add_action('wp_enqueue_scripts', 'trustpilot_enqueue_scripts');
+
+
+
+
+/*
+WP CLEANUP
+*/
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+ add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param array $plugins 
+ * @return array Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+ if ( is_array( $plugins ) ) {
+ return array_diff( $plugins, array( 'wpemoji' ) );
+ } else {
+ return array();
+ }
+}
+
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed for.
+ * @return array Difference betwen the two arrays.
+ */
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+ if ( 'dns-prefetch' == $relation_type ) {
+ /** This filter is documented in wp-includes/formatting.php */
+ $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+$urls = array_diff( $urls, array( $emoji_svg_url ) );
+ }
+
+return $urls;
+}
+// Remove dashicons in frontend for unauthenticated users
+add_action( 'wp_enqueue_scripts', 'bs_dequeue_dashicons' );
+function bs_dequeue_dashicons() {
+    if ( ! is_user_logged_in() ) {
+        wp_deregister_style( 'dashicons' );
+    }
+}
---
+++ dist/js/scripts.js
@@ -0,0 +1,4 @@
+/*! lmcu-divi-child-theme v0.0.1 | (c) 2021 Ben West | GPL License | https://github.com/lmculondon/lmcu-divi-child-theme */
+jQuery(document).ready((function ($) {
+  // Code Here
+}));

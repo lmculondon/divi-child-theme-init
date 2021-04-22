c105574e99e6c846083faa564aaf7f20bae4a0ff
benjeewest <ben@benwest.me>
2021-04-22 09:59:11 +0100

    Remove LMCU Calculator CSS

--- src/copy/functions.php
+++ src/copy/functions.php
@@ -9,14 +9,10 @@ SCRIPTS AND STYLES
 function theme_enqueue_styles() {
 //	wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'lmcu-style', get_stylesheet_directory_uri() . '/css/style.css', array());
-	//wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Montserrat:wght@400;600;700&display=swap', false );
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,500i,700|Montserrat:400,600,700&display=swap', false );
-
     wp_enqueue_script( 'lmcu-custom', get_stylesheet_directory_uri() . '/js/scripts.min.js', array( 'jquery', 'divi-custom-script' ), true );
-	
 	}
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-
 
 function remove_divi_customizer_styles() {
     remove_action( 'wp', 'et_divi_add_customizer_css' );
@@ -24,14 +20,13 @@ function remove_divi_customizer_styles() {
 add_action( 'wp', 'remove_divi_customizer_styles', 999 ); 
 
 //Preconnect Gfonts
-function gfont_preload() {
+function font_preload() {
 	echo '<link rel="preconnect" href="https://fonts.gstatic.com">';
     $upload_dir = wp_upload_dir();
-
     echo '<link rel="preload" as="font" href="' . get_template_directory_uri() . '/core/admin/fonts/modules.ttf" type="font/ttf" crossorigin="anonymous">';
     echo '<link rel="preload" as="font" href="' . $upload_dir['baseurl'] . '/divi-uploaded-icons-diui-awb/lmcu-icons/lmcu-icons.ttf" type="font/ttf" crossorigin="anonymous">';								
 }
-add_action('wp_head', 'gfont_preload');
+add_action('wp_head', 'font_preload');
 
 // Prevent default font load
 function prevent_loading_fonts() {
@@ -39,31 +34,25 @@ function prevent_loading_fonts() {
 }
 add_action( 'init', 'prevent_loading_fonts', 20 );
 
-
-
 //TrustPilot
 function trustpilot_enqueue_scripts() {
 	wp_enqueue_script( 'trustpilot', '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js', array(),false, true);
 }
 add_action('wp_enqueue_scripts', 'trustpilot_enqueue_scripts');
 
-
-
-
 /*
 WP CLEANUP
 */
 function dequeue_redundant_styles() {
     wp_dequeue_style( 'dlm-frontend' );
+    wp_dequeue_style( 'calculator-css');
     wp_dequeue_style('divi-upload-icons-awb-public-css');
     wp_dequeue_style('divi-upload-icons-awb-icon-filter-css');
-    
-
 }
 add_action( 'wp_enqueue_scripts', 'dequeue_redundant_styles', 999 );	
 
-//Remove JQuery migrate
-function remove_jquery_migrate($scripts)
+//Remove JQuery migrate 
+/*function remove_jquery_migrate($scripts)
 {
     if (!is_admin() && isset($scripts->registered['jquery'])) {
         $script = $scripts->registered['jquery'];
@@ -74,7 +63,7 @@ function remove_jquery_migrate($scripts)
             ));
         }
     }
-}
+}*/
 
 //add_action('wp_default_scripts', 'remove_jquery_migrate');
 /**
